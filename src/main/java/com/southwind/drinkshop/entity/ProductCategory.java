c3d2f0b93f36b7f48d84c3a635cddb90db2b12ef
Marrria0126 <yihongxia0126@gmail.com>
2021-03-09 00:09:15 +0800

    终于写完啦

--- src/main/java/com/southwind/drinkshop/entity/ProductCategory.java
+++ src/main/java/com/southwind/drinkshop/entity/ProductCategory.java
@@ -1,8 +1,12 @@
 package com.southwind.drinkshop.entity;
 
 import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
  * <p>
@@ -14,10 +18,12 @@ import lombok.EqualsAndHashCode;
  */
 @Data
   @EqualsAndHashCode(callSuper = false)
+  @Accessors(chain = true)
     public class ProductCategory implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+  @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     private String name;
