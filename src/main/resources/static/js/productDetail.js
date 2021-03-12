54a2dbab6d701c2002405df2ca05c1f1d9348c9c
Marrria0126 <yihongxia0126@gmail.com>
2021-03-12 09:04:54 +0800

    p7完成，视频44:50中alert有问题（始终存在）

--- src/main/resources/static/js/productDetail.js
+++ src/main/resources/static/js/productDetail.js
@@ -37,9 +37,13 @@ function reduce() {
 }
 
 //添加购物车
-function addCart(id,price){
-    var id = $("#productId").val();
-    var price = $("#productPrice").val();
-    var quantity = $("#quantity").val();
-    window.location.href="settlement1.html";
+function addCart(productId,price){
+    let stockStr = $("#stock").text();
+    let stock = parseInt(stockStr);
+    if(stock == 0){
+        alert("库存不足！");
+        return false;
+    }
+    let quantity = $("#quantity").val();
+    window.location.href="/cart/add/"+productId+"/"+price+"/"+quantity;
 }
